4883f9d0663f68984086f1fb150a93bc0de963a0
krrishmahar <krrishmahar5@gmail.com>
2026-03-14 21:11:35 +0530

    gaming sponsor added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-   theme: {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
     extend: {
       fontFamily: {
         inter: ["Inter", "Manrope", "sans-serif"],
         manrope: ["manrope", "sans-serif"],
         harry: ["harryP", "serif"],
         wizard: ["blackchancery", "serif"],
-      }
+      },
+      KeyframeEffects: {
+        "fade-in": {
+          "0%": { opacity: "0" },
+        },
+        shine: {
+          "0%": { left: "-100%" },
+          "100%": { left: "100%" },
+        },
+      },
     },
+    plugins: [],
   },
-  plugins: [],
-}
+};
